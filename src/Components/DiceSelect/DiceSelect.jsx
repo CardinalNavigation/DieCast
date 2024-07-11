0e345a7b3deea82506ca3197a265f8946b04cf34
Matthew Johnson <mtthw.jhnson@gmail.com>
2024-07-10 20:58:41 -0500

    Working out issues with CSS styling.

--- src/Components/DiceSelect/DiceSelect.jsx
+++ src/Components/DiceSelect/DiceSelect.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
+import Result from "../Result/Result"
+import History from "../History/History"
 
-//Here we will start with just once dice at a time.
-//One button press. One Result
+//DiceSelect is the Buttons at the bottom of the screen
+//Buttons are clicked here, state is captured and passed as a singular dice value.
+// To Do: Create a toggle that allows users to roll multiple dice at once
+// To Do: Toggle should also allow users to roll different dice kinds
+
 function DiceSelect() {
 
     const [diceValue, setDiceValue] = useState(null)
-
-    console.log(diceValue)
 
     const diceRoll = (dice) => {
         let roll = Math.floor(Math.random() * dice) + 1
@@ -14,15 +17,16 @@ function DiceSelect() {
     }
 
     return (
-        <>
-            <p>{diceValue}</p>
+        <div>
+            <Result result={diceValue} />
             <button onClick={() => diceRoll(4)}>d4</button>
             <button onClick={() => diceRoll(6)}>d6</button>
             <button onClick={() => diceRoll(8)}>d8</button>
             <button onClick={() => diceRoll(10)}>d10</button>
             <button onClick={() => diceRoll(12)}>d12</button>
             <button onClick={() => diceRoll(20)}>d20</button>
-        </>
+            <button onClick={() => diceRoll(100)}>d100</button>
+        </div>
     )
 }
 
